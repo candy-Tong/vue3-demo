5d9148cf7343d0f4c6d511822a159a25f4c8f334
candy-Tong <563378816@qq.com>
2022-01-05 09:48:45 +0800

    feat: 新增递归 demo

---
+++ demos/queue-recurse-demo/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <AddOne :count='count' @add='add' />
+</template>
+<script setup lang='ts'>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import AddOne from './components/AddOne.vue'
+import { ref } from 'vue'
+
+const count = ref(0)
+
+function add() {
+  count.value = count.value + 1
+}
+</script>
+
+
---
+++ demos/queue-recurse-demo/src/components/AddOne.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>{{ count }}</h1>
+  <button @click="emit('add')">AddOne</button>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+defineProps<{ count: number }>()
+const emit = defineEmits(['add'])
+</script>
